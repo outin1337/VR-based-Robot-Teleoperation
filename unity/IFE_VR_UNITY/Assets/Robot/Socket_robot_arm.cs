4f996562b12b267426665ab2cce2fee74bd0869f
Fredrik Dahl <fredrida@hiof.no>
2023-11-06 15:21:50 +0100

    Updated socket code

--- unity/IFE_VR_UNITY/Assets/Robot/Socket_robot_arm.cs
+++ unity/IFE_VR_UNITY/Assets/Robot/Socket_robot_arm.cs
@@ -7,8 +7,8 @@ using System.Globalization;
 
 public class Socket_robot_arm : MonoBehaviour
 {
-    private IPAddress robotIP = IPAddress.Parse("192.168.56.1");
-    private int port = 30002;
+    private IPAddress robotIP = IPAddress.Parse("158.39.163.5");
+    private int port = 5005;
     private TcpListener tcpListener;
 
     void Start()
