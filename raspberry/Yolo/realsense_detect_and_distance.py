0185048a4fdb63e64014d16a51175f2d86ff297d
outin1337 <gangstaleesho@gmail.com>
2024-01-16 15:25:04 +0100

    Realsense aligned images

---
+++ raspberry/Yolo/realsense_detect_and_distance.py
@@ -0,0 +1,102 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator
+import random
+import torch
+
+class CameraStream:
+
+    def __init__(self, DEBUG=True):
+        self.DEBUG = DEBUG
+        if DEBUG: print('WARNING: Debug is on')
+        self.RUN = True
+
+        self.model = YOLO('yolov8n.pt')
+
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+
+        self.pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+        self.pipeline_profile = self.config.resolve(self.pipeline_wrapper)
+        self.device = self.pipeline_profile.get_device()
+        self.device_product_line = str(self.device.get_info(rs.camera_info.product_line))
+
+        self.found_rgb = False
+        for s in self.device.sensors:
+            if s.get_info(rs.camera_info.name) == 'RGB Camera':
+                self.found_rgb = True
+                break
+        if not self.found_rgb:
+            print("The demo requires Depth camera with Color sensor")
+            exit(0)
+        
+    def config(self, height=640, width=480, fps=30):
+        self.config.enable_stream(rs.stream.depth, height, width, rs.format.z16, fps)
+
+        if self.device_product_line == 'L500':
+            self.config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+        else:
+            self.config.enable_stream(rs.stream.color, height, width, rs.format.bgr8, fps)
+
+        self.pipeline.start(self.config)
+    
+    def stream(self):
+        try:
+            while self.RUN:
+                self.align = rs.align(rs.stream.color)
+                self.frames = self.pipeline.wait_for_frames()
+                self.aligned_frames = self.align.process(self.frames)
+
+                self.color_frame = self.aligned_frames.first(rs.stream.color)
+                self.depth_frame = self.aligned_frames.get_depth_frame()
+
+                if not self.depth_frame or not self.color_frame:
+                    continue
+
+                self.depth_image = np.asanyarray(self.depth_frame.get_data())
+                self.color_image = np.asanyarray(self.color_frame.get_data())
+
+
+                self.results = self.model.predict(self.color_image, conf=0.7, verbose=False)
+                self.annotator = Annotator(self.color_image)
+
+                for r in self.results:
+                  self.boxes = r.boxes
+                  for box in self.boxes:
+                      b = box.xyxy[0]
+                      c = box.cls
+                      x1, y1, x2, y2 = b.numpy()
+                      x, y = (x1+x2)/2, (y1+y2)/2
+                      self.label = self.model.names[int(c)] + " " + str(round(self.depth_frame.get_distance(int(x), int(y)), 2)) + "m"
+                      random.seed(int(c))
+                      self.annotator.box_label(b, label=self.label, color=(random.randint(0,255),random.randint(0,255),random.randint(0,255)))
+                      if self.DEBUG:
+                        self.annotator.box_label(torch.tensor([x,y+1, x,y+1]), "(" + str(round(x,2)) +", "+ str(round(y,2)) + ")")
+
+                self.depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(self.depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+                self.depth_colormap_dim = self.depth_colormap.shape
+                self.color_colormap_dim = self.color_image.shape
+
+                if self.depth_colormap_dim != self.color_colormap_dim:
+                    self.resized_color_image = cv2.resize(self.color_image, dsize=(self.depth_colormap_dim[1], self.depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+                    images = np.hstack((self.resized_color_image, self.depth_colormap))
+                else:
+                    images = np.hstack((self.color_image, self.depth_colormap))
+
+                if self.DEBUG:
+                    cv2.namedWindow('RealSense')
+                    cv2.imshow('RealSense', images)
+                    self.key = cv2.waitKey(1)
+                
+                if self.key & 0xFF == ord('q') or self.key & 0xFF == ord('Q'):
+                        RUN = False
+                        cv2.destroyAllWindows()
+                        break
+                
+                return self.resized_color_image
+
+        finally:
+            self.pipeline.stop()
---
+++ raspberry/Yolo/realsense_test_old.py
@@ -0,0 +1,123 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator
+import random
+import torch
+
+DEBUG = True
+RUN = True
+ONCE = True
+
+model = YOLO('yolov8n.pt')
+
+pipeline = rs.pipeline()
+config = rs.config()
+
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+pipeline.start(config)
+
+try:
+    while RUN:
+        align = rs.align(rs.stream.color)
+        
+        frames = pipeline.wait_for_frames()
+        aligned_frames = align.process(frames)
+
+        color_frame = aligned_frames.first(rs.stream.color)
+        depth_frame = aligned_frames.get_depth_frame()
+
+        """
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        """
+        
+        if not depth_frame or not color_frame:
+            continue
+
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        
+        results = model.predict(color_image, conf=0.7, verbose=False)
+        annotator = Annotator(color_image)
+        #if results[0].masks is not None:
+        #    clss = results[0].boxes.cls.cpu().tolist()
+        #    masks = results[0].masks.xy
+        #    for ind, mask, cls in zip(range(len(masks)), masks, clss):
+        #        arr_len = len(masks[ind])
+        #        #x, y = masks[ind][arr_len//8][0], masks[ind][arr_len//8][1]
+        #        x, y = masks[ind][0][0], masks[ind][0][1]
+        #        annotator.seg_bbox(mask=mask,
+        #                           det_label=model.model.names[int(cls)] + " " + str(round(depth_frame.get_distance(int(x), int(y)), 2)) + "m")
+        #        if DEBUG:
+        #            annotator.box_label(torch.tensor([x,y+15, x,y+16]), "(" + str(x) +", "+ str(y) + ")")
+
+        if ONCE:
+            print(results)
+            ONCE = False
+
+        for r in results:
+          
+          boxes = r.boxes
+          for box in boxes:
+              b = box.xyxy[0]
+              c = box.cls
+              x1, y1, x2, y2 = b.numpy()
+              x, y = (x1+x2)/2, (y1+y2)/2
+              label = model.names[int(c)] + " " + str(round(depth_frame.get_distance(int(x), int(y)), 2)) + "m"
+              random.seed(int(c))
+              annotator.box_label(b, label=label, color=(random.randint(0,255),random.randint(0,255),random.randint(0,255)))
+              if DEBUG:
+                annotator.box_label(torch.tensor([x,y+1, x,y+1]), "(" + str(round(x,2)) +", "+ str(round(y,2)) + ")")
+              #print(model.names[int(c)], b)
+        #yolo_frame = results[0].plot()
+        #yolo_frame = annotator.result()
+
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        depth_colormap_dim = depth_colormap.shape
+        color_colormap_dim = color_image.shape
+
+        if depth_colormap_dim != color_colormap_dim:
+            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+            images = np.hstack((resized_color_image, depth_colormap))
+        else:
+            images = np.hstack((color_image, depth_colormap))
+
+        if DEBUG:
+            cv2.namedWindow('RealSense')
+            cv2.imshow('RealSense', images)
+            key = cv2.waitKey(1)
+        else:
+            pass
+            
+        
+        if key & 0xFF == ord('q') or key & 0xFF == ord('Q'):
+                RUN = False
+                cv2.destroyAllWindows()
+                break
+
+finally:
+    pipeline.stop()
