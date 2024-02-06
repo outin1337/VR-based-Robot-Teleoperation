7e67a4c56501eef5d71164bdbf2ec67c703cde91
outin <gangstaleesho@gmail.com>
2024-02-06 12:17:04 +0100

    Added a main scene

--- unity/IFE_VR_UNITY/Assets/Utils/MoveObjectFront.cs
+++ unity/IFE_VR_UNITY/Assets/Utils/MoveObjectFront.cs
@@ -3,18 +3,29 @@ using UnityEngine;
 public class MoveObjectFront : MonoBehaviour
 {
     public Camera mainCamera;
-    public float distanceFromCamera = 2.0f;
-    public float horizontalOffset = 1.0f;
-    public bool isLeftQuad = true; 
-
+    public float distanceFromCamera = 0.0f;
+    public float horizontalOffset = 0.0f;
+    public float verticalOffset = 0.0f;
+    public bool isLeftQuad = true;
+    public bool samePos = false;
+    
     private void Update()
     {
-        transform.rotation = mainCamera.transform.rotation;
+        Transform cameraTransform = mainCamera.transform;
+        if (samePos)
+        {
+            transform.SetPositionAndRotation(cameraTransform.position, cameraTransform.rotation);
+        }
+        else
+        {
+            transform.rotation = cameraTransform.rotation;
 
-        Vector3 offsetDirection = isLeftQuad ? -mainCamera.transform.right : mainCamera.transform.right;
+            Vector3 offsetDirection = isLeftQuad ? -cameraTransform.right : cameraTransform.right;
 
-        transform.position = mainCamera.transform.position
-                           + mainCamera.transform.forward * distanceFromCamera
-                           + offsetDirection * horizontalOffset;
+            transform.position = cameraTransform.position
+                                 + cameraTransform.forward * distanceFromCamera
+                                 + offsetDirection * horizontalOffset
+                                 + cameraTransform.up * verticalOffset;
+        }
     }
 }
